311c5900ec96f27fcb11ebe9c1b442836f77cc08
Christie Renzetti <christie.renzetti@gmail.com>
2022-03-22 17:54:18 -0500

    Week 5 - Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,10 +21,9 @@ class CommentForm extends React.Component {
   }
 
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
-
-  }
+    this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+}
 
   render() {
     return (
@@ -113,7 +112,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -127,7 +126,7 @@ function RenderComments({ comments }) {
             </div >
           );
         })}
-        <CommentForm addComment={comments} campsiteId={CampsiteInfo} />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -150,7 +149,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
         </div>
       </div>
     );
